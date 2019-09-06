76f0f135171b9bda10cf39a9fdc11bb5c30f738c
Nick McCurdy <nick@nickmccurdy.com>
2019-09-06 11:31:52 -0400

    Fix ESLint syntax in 2019 config

--- es2019.js
+++ es2019.js
@@ -1,5 +1,5 @@
 module.exports = {
   extends: "./es2018",
   parserOptions: { ecmaVersion: 10 },
-  rules: { "no-unused-vars": { caughtErrors: "all" } }
+  rules: { "no-unused-vars": ["error", { caughtErrors: "all" }] }
 };
