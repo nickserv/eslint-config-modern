559331afe2ccdb2fb2978f2174b87d5a793ef982
Nick McCurdy <nick@nickmccurdy.com>
2022-07-29 01:47:38 -0400

    Format categories.js

--- categories.js
+++ categories.js
@@ -1,31 +1,31 @@
-"use strict";
-const config = require(".");
-const { Linter } = require("eslint");
-const { readFile } = require("fs/promises");
-const { groupBy } = require("lodash");
+"use strict"
+const config = require(".")
+const { Linter } = require("eslint")
+const { readFile } = require("fs/promises")
+const { groupBy } = require("lodash")
 
 async function getDocumentedRules() {
-  const markdown = await readFile("README.md", "utf8");
-  const headingMatches = markdown.matchAll(/^#.*\((?<rules>.*)\)$/gmu);
+  const markdown = await readFile("README.md", "utf8")
+  const headingMatches = markdown.matchAll(/^#.*\((?<rules>.*)\)$/gmu)
   return Array.from(headingMatches).flatMap(({ groups: { rules } }) => {
-    const ruleMatches = rules.matchAll(/`(?<rule>[a-z-]+)`/gu);
-    return Array.from(ruleMatches).map(({ groups: { rule } }) => rule);
-  });
+    const ruleMatches = rules.matchAll(/`(?<rule>[a-z-]+)`/gu)
+    return Array.from(ruleMatches).map(({ groups: { rule } }) => rule)
+  })
 }
 
 function getRulesByCategory() {
-  const allRules = new Linter().getRules();
+  const allRules = new Linter().getRules()
   return groupBy(
     Object.keys(config.rules),
-    (rule) => allRules.get(rule).meta.docs.category
-  );
+    (rule) => allRules.get(rule).meta.docs.category,
+  )
 }
 
-(async () => {
-  const documentedRules = await getDocumentedRules();
+;(async () => {
+  const documentedRules = await getDocumentedRules()
 
   function countDocumentedRules(rules) {
-    return rules.filter((rule) => documentedRules.includes(rule)).length;
+    return rules.filter((rule) => documentedRules.includes(rule)).length
   }
 
   console.log(
@@ -36,15 +36,15 @@ function getRulesByCategory() {
             categoryRules.length
           })`,
           ...categoryRules.map(
-            (rule) => `${documentedRules.includes(rule) ? "✓" : "✖"} ${rule}`
+            (rule) => `${documentedRules.includes(rule) ? "✓" : "✖"} ${rule}`,
           ),
-        ].join("\n")
+        ].join("\n"),
       )
       .concat(
         `Total (${countDocumentedRules(Object.keys(config.rules))}/${
           Object.keys(config.rules).length
-        })`
+        })`,
       )
-      .join("\n\n")
-  );
-})();
+      .join("\n\n"),
+  )
+})()
