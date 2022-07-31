37970b865049727eb7aa7d323174b8b0bbd63537
Nick McCurdy <nick@nickmccurdy.com>
2022-07-30 22:31:22 -0400

    Move config files for consistency

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+"use strict"
+
+module.exports = {
+  extends: ".",
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    sourceType: "script",
+  },
+  rules: {
+    "sort-keys": "error",
+  },
+}
