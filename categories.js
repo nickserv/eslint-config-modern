d6e6bc38e5205767c6225612561c85869595741e
Nick McCurdy <nick@nickmccurdy.com>
2022-08-02 08:01:17 -0400

    Merge scripts and show rules as a table

--- categories.js
+++ categories.js
@@ -1,8 +1,8 @@
 "use strict"
 const config = require(".")
 const { Linter } = require("eslint")
+const prettier = require("eslint-config-prettier")
 const { readFile } = require("fs/promises")
-const { groupBy } = require("lodash")
 
 ;(async () => {
   const markdown = await readFile("README.md", "utf8")
@@ -15,32 +15,26 @@ const { groupBy } = require("lodash")
   )
 
   const allRules = new Linter().getRules()
-  const rulesByCategory = groupBy(
-    Object.keys(config.rules),
-    (rule) => allRules.get(rule).meta.docs.category,
-  )
 
-  function countDocumentedRules(rules) {
-    return rules.filter((rule) => documentedRules.includes(rule)).length
-  }
+  const prettierRules = Object.entries(prettier.rules)
+    .filter(([, value]) => value === "off")
+    .map(([key]) => key)
 
-  console.log(
-    Object.entries(rulesByCategory)
-      .map(([category, categoryRules]) =>
-        [
-          `${category} (${countDocumentedRules(categoryRules)}/${
-            categoryRules.length
-          })`,
-          ...categoryRules.map(
-            (rule) => `${documentedRules.includes(rule) ? "✅" : "❌"} ${rule}`,
-          ),
-        ].join("\n"),
-      )
-      .concat(
-        `Total (${countDocumentedRules(Object.keys(config.rules))}/${
-          Object.keys(config.rules).length
-        })`,
-      )
-      .join("\n\n"),
+  console.table(
+    Object.fromEntries(
+      Array.from(allRules.entries()).map(([rule, { meta }]) => {
+        const { category } = meta.docs
+        let status = "To-do"
+        if (prettierRules.includes(rule)) {
+          status = "Prettier"
+        } else if (documentedRules.includes(rule)) {
+          status = "Documented"
+        } else if (rule in config.rules) {
+          status = "Implemented"
+        }
+        const docs = `https://eslint.org/docs/latest/rules/${rule}`
+        return [rule, { category, status, docs }]
+      }),
+    ),
   )
 })()
